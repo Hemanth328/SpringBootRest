eefe96f1c2fcbd43b6372167b3dbf1133b862fb3
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-10-15 18:32:44 +0530

    Sending data through @RequestParam

--- SpringBootRestProj07-SendingDataAsRequestParam/src/main/java/com/org/rest/CustomerOperationsController.java
+++ SpringBootRestProj07-SendingDataAsRequestParam/src/main/java/com/org/rest/CustomerOperationsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.PostExchange;
 
 @RestController
-@RequestMapping("/customercontro")
+@RequestMapping("/customercontroller")
 public class CustomerOperationsController {
 
 	/*@GetMapping("/report")
